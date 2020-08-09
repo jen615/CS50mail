770b3b4108b533fe87d4d83ec11ec8ea3198ea6f
jen615 <xjenghisx@gmail.com>
2020-08-08 17:21:27 -0700

    the archive option on the email page would work better when working with components rather than html objects in vanilla js. abandoning concept, saving for reference.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -28,6 +28,13 @@ document.addEventListener('DOMContentLoaded', function() {
           load_mailbox('sent')
         })
   })
+
+  // Add event listener for archive buttons
+  document.querySelectorAll('.archive-check').forEach((email) => {
+    email.addEventListener('click', () =>{
+      email
+    })
+  })
 });
 
 
@@ -48,25 +55,89 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   // Fetch the mail
   fetch(`/emails/${mailbox}`)
       .then(response => response.json())
       .catch(error => {
-        console.log('FUCK')
+        console.log('FUCK:', error)
       })
       .then(emails => {
-        const emailList = document.createElement('ul')
-        emailList.id = 'list'
-        document.querySelector('#emails-view').append(emailList)
-        let email;
-        for (email in emails) {
-          const mail = document.createElement('li')
-          mail.innerHTML = email
-          document.querySelector('#list').append(mail)
+        console.log(emails)
+        // Build Email Table
+        mailTable()
+        // Populate Email Table
+
+        if (mailbox === 'inbox' || 'sent') {
+          for (const email of emails) {
+            // noinspection JSUnresolvedVariable
+            if (email.archived) {
+              continue
+            }
+            renderEmail(mailbox, email)
+          }
         }
       })
+}
+
+function mailTable(mailbox) {
+
+  // Make Email Table
+  const emailList = document.createElement('table')
+  emailList.id = 'e-table'
+  const header = emailList.createTHead();
+  const row = header.insertRow(0);
+
+  // Table Headers
+  const sender = row.insertCell(0);
+  sender.innerHTML = '<b> Sender </b>'
+  const time = row.insertCell(1);
+  time.innerHTML = '<b> Time </b>'
+  const subject = row.insertCell(2);
+  subject.innerHTML = '<b> Subject </b>'
+  const body = row.insertCell(3);
+  body.innerHTML = '<b> Body </b>'
+
+  if (mailbox === 'inbox' || 'archive') {
+    const archive = row.insertCell(4);
+    archive.innerHTML = '<b> Archive </b>'
+  }
+
+  // Add table to DOM
+  document.querySelector('#emails-view').append(emailList)
+}
+
+function renderEmail(mailbox, email) {
+
+  // Create new row for the email
+  const mail = document.createElement('tr');
+
+  if (email.read) {
+    mail.className = 'read'
+  }
+  mail.className = 'email'
+
+  const sender = mail.insertCell(0);
+  sender.innerHTML = email.sender;
+  const time = mail.insertCell(1);
+  time.innerHTML = email.timestamp;
+  const subject = mail.insertCell(2);
+  subject.innerHTML = email.subject;
+  const body = mail.insertCell(3);
+  body.innerHTML = email.body;
+  // Add archive option for inbox & archive box
+  if (mailbox === 'inbox' || 'archive') {
+    const archive = mail.insertCell(4);
+    const check = document.createElement('input')
+    check.type = 'checkbox'
+    check.className = 'archive-check'
+    check.value = email.archived
+    archive.append(check)
+  }
+  document.querySelector('#e-table').append(mail)
+}
+
+function archiver() {
 
 }
